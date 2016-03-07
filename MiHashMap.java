2a2f23a12af17f65a0c2c7a50af6a4e875a52259
unknown <you@example.com>
2016-03-07 12:34:26 +0100

    Terminado constructor sin parametros

---
+++ MiHashMap.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    private int[] valores;
+    private String[] claves;
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        valores = new int [0];
+        claves = new String[0];
+    }
+}
